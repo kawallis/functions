41e816a70321ed65732025c88df023fdaa397e90
Chris_Wallace <kawallis@icloud.com>
2017-02-10 15:14:26 -0800

    made the fourth function

--- lab/app.js
+++ lab/app.js
@@ -88,13 +88,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-  
+  var a = testArray[0];
+  var b = testArray[1];
+  var c = testArray[2];
+  var sum = sumAndMultiply(a,b,c)[0]; 
+  var message = a.toString() + ',' + b.toString() + ',' + c.toString() + ' was passed in as an array of numbers, and ' + sum + ' is their sum.'
+  return[sum, message];
 }
 //sumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
